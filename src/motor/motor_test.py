f67f4fbbbe8e2ef77fdc77e99ec9f7296c4e2fdb
Alexander Schmitz <Alexander.Schmitz95@web.de>
2026-09-11 19:30:09 +0200

    Update servo zero positions and angle limits for modified arm geometry

--- src/motor/motor_test.py
+++ src/motor/motor_test.py
@@ -10,10 +10,7 @@ for servo_ID in range(2, 8):
     pk.write1ByteTxRx(servo_ID, 40, 1)
 
 def test():
-    move_to_angles(pk, [0,20,60,0,40,0])
-    move_by_angles(pk, (20,20,20,20,20,20))
-    move_by_angles(pk, (-40,-40,-40,-40,-40,0))
-    move_by_angles(pk, (20,20,20,20,20,-20))
+    move_to_angles(pk, [0 , 10 , 10 , -90 , 0 , 90])
 
 
 if __name__ == "__main__":
